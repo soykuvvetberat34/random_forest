228142338738da9bf771db45bd24325237141e98
Berat SOYKUVVET <69586522+soykuvvetberat34@users.noreply.github.com>
2024-07-17 22:56:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 datas=pd.read_csv("C:\\Users\\berat\\pythonEğitimleri\\python\\Makine Öğrenmesi\\random forest\\aylara_gore_satis.csv")
 months=datas.iloc[:,0].values.reshape(-1,1)
 sales=datas.iloc[:,1].values.reshape(-1,1)
-RandomFR=RandomForestRegressor()
+RandomFR=RandomForestRegressor(n_estimators=10,random_state=0)
 RandomFR.fit(months,sales)
 predict=RandomFR.predict(months)
 plt.scatter(months,sales)
